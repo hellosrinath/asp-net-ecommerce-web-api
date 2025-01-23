afdf9cc39b64d3f30f34efa0bf44500ca50ec89f
hellosrinath <s.nath32@yahoo.com>
2025-01-24 02:46:28 +0600

    make crud with category class

--- Program.cs
+++ Program.cs
@@ -15,37 +15,78 @@ if (app.Environment.IsDevelopment()){
 app.UseHttpsRedirection();
 
 // REST API: => GET, POST, PUT, DELETE
+List<Category> categories = new List<Category>();
 
 app.MapGet("/", () =>  "Api is working fine");
 
-app.MapGet("/hello", () => "Get Method: Hello");
-
-// return list of products
-var products = new List<Product>(){
-    new Product("Samsung s20", 44000),
-    new Product("iPhone 14", 150000),
-    new Product("Mac Mini", 120000)
-};
-
-app.MapGet("/products", () => Results.Ok(products));
-
-// return html
-app.MapGet("/html", () =>  Results.Content("<h1>Hello, HTML</h1>","text/html"));
-
-app.MapPost("/hello", () => {
-    // return json object
-    var response = new {message = "Data successfully updated", success = true};
-    return Results.Ok(response);
+app.MapPost("/api/v1/categories", () => 
+{
+    var newCategory = new Category{
+        CategoryId = Guid.Parse("df5a7127-a044-49c2-b977-f47ad18c52fb"),
+        Name = "Smart Phones",
+        Description = "Smart Phone is a nice categories",
+        CreatedAt = DateTime.UtcNow,
+    };
+    categories.Add(newCategory);
+    return Results.Created($"/api/v1/categories/{newCategory.CategoryId}",newCategory);
 });
 
-app.MapPut("/hello", () => {
-    return Results.Accepted();  
-});
 
-app.MapDelete("/hello", () => {
+app.MapDelete("/api/v1/categories", () =>
+{
+    var foundCategory = categories.FirstOrDefault(c => c.CategoryId 
+    == Guid.Parse("df5a7127-a044-49c2-b977-f47ad18c52fb"));
+
+    if (foundCategory == null){
+        return Results.NotFound("Category with this id does not exits");
+    }
+    
+    categories.Remove(foundCategory);
+
     return Results.NoContent();
+
 });
+
+app.MapPut("/api/v1/categories", () =>
+{
+    var foundCategory = categories.FirstOrDefault(c => c.CategoryId 
+    == Guid.Parse("df5a7127-a044-49c2-b977-f47ad18c52fb"));
+
+    if (foundCategory == null){
+        return Results.NotFound("Category with this id does not exits");
+    }
+    
+    foundCategory.Name = "Electronics";
+    foundCategory.Description = "This is good categories";
+    
+    return Results.NoContent();
+
+});
+
+
+app.MapGet("/api/v1/categories", () =>
+{
+
+    return Results.Ok(categories);
+
+});
+
+
 
 app.Run();
 
-public record Product(string ProductName, decimal Price); 
+public record Category {
+    public Guid CategoryId {get; set;}
+    public string? Name {get; set;}
+    public string? Description {get; set;}
+    public DateTime CreatedAt {get; set;}
+};
+
+/*
+    CRUD
+
+    Create => Create a category => POST: /api/v1/categories
+    Read   => Read a category => GET: /api/v1/categories
+    Update => Update a category => POST:/api/v1/categories
+    Delete => Delete a category => Delete: /api/v1/categories
+*/
