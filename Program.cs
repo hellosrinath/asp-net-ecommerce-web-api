060511d9b6435808e84c8425664d9267d8ec92c0
hellosrinath <s.nath32@yahoo.com>
2025-01-23 23:10:26 +0600

    testing End point: GET, POST, PUT, Delete by rest client vs code extensions

--- Program.cs
+++ Program.cs
@@ -4,4 +4,26 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+// REST API: => GET, POST, PUT, DELETE
+
+app.MapGet("/", () => {
+    return "Api is working fine";
+});
+
+app.MapGet("/hello", () => {
+    return "Get Method: Hello";
+});
+
+app.MapPost("/hello", () => {
+    return "Post Method: Hello";
+});
+
+app.MapPut("/hello", () => {
+    return "Put Method: Hello";
+});
+
+app.MapDelete("/hello", () => {
+    return "Delete Method: Hello";
+});
+
 app.Run();
